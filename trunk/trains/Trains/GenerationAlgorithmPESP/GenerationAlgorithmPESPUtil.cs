6ba1cb938637ab03dc92168ece79e1b988970856
kalicky <kalicky@62c59678-76e0-11de-91a4-c5220aa94298>
2009-07-23 15:33:30 +0000

    Update: Implemented methods, which create constraint set. (SameTT, AlfaTTT, FullDS).

--- trunk/trains/Trains/GenerationAlgorithmPESP/GenerationAlgorithmPESPUtil.cs
+++ trunk/trains/Trains/GenerationAlgorithmPESP/GenerationAlgorithmPESPUtil.cs
@@ -166,70 +166,141 @@ namespace PeriodicTimetableGeneration
 		public static List<int> createSequenceOfNumber(int length)
 		{
 			return createSequenceOfNumber(0, length - 1);
-		}
+        }
 
-		/// <summary>
-		/// Creates the Sets for constraints.
+        #endregion
+
+
+        #region Create Constraint Sets' Methods
+
+        /// <summary>
+		/// Creates the Sets for constraints with the same size of discrete set.
+        /// Therefore same maximal transfer time is quaranteed for all transfers.
 		/// </summary>
 		/// <param name="constraints">The constraints.</param>
 		/// <param name="size">The size of set.</param>
 		/// <returns>List of constraints.</returns>
-		public static List<Constraint> createConstraintSets(List<Constraint> constraints, int size)
+		public static List<Constraint> createConstraintSets_SameTransferTime(List<Constraint> constraints, int size)
 		{
 			foreach (Constraint constraint in constraints)
 			{
-				createConstraintSet(constraint, size);
+				createConstraintSet_SameTransferTime(constraint, size);
 			}
 			return constraints;
 		}
 
 		/// <summary>
 		/// Creates the Set for constraint.
+        /// Calculate with frequency of within Modulo Model.
+        /// Maximal transfer time is then same for all transfers.
 		/// </summary>
 		/// <param name="constraint">The constraint.</param>
 		/// <param name="size">The size of set.</param>
-		public static void createConstraintSet(Constraint constraint, int size)
+		public static void createConstraintSet_SameTransferTime(Constraint constraint, int size)
 		{
-			// create discrete set according the sequence of number of length "size"
-			constraint.DiscreteSet = new Set(createSequenceOfNumber(size), MODULO_DEFAULT);
-			// create minimization factor for appropriate discrete set
-			// according the amount of passenger on transfer
-			constraint.DiscreteSet.createMinimizationFactor(constraint.Transfer.Passengers);
+            List<Set> newSets = new List<Set>();
+            // get minimal period
+            int period = Math.Min((int)constraint.TrainLine1.Period, (int)constraint.TrainLine2.Period);
+            // calculate frequency
+            int frequency = MODULO_DEFAULT / period;
+            // create as many seqence as they are within modulo
+            for (int i = 0; i < MODULO_DEFAULT / period; i++)
+            {
+                // create discrete set {60*i .. 60*i + size -1}
+                Set newSet = new Set(createSequenceOfNumber(i*period, i*period + size - 1), MODULO_DEFAULT);
+                // create minimization factor for appropriate discrete set
+                // according the amount of passenger on transfer
+                newSet.createMinimizationFactor(constraint.Transfer.Passengers);
+                // add newSet
+                newSets.Add(newSet);
+            }
+
+            Set finalSet = null;
+			Boolean first = true;
+			// union sets
+			foreach (Set set in newSets)
+			{
+				// if first, just set as a final
+				if (first)
+				{
+					finalSet = set;
+					first = false;
+				}
+				// otherwise unions with next one
+				else
+				{
+					finalSet.UnionWith(set);
+				}
+			}
+			// finally initialize Set for Constraint
+			constraint.DiscreteSet = finalSet;
 		}
 
+
+        /// <summary>
+        /// Creates the Sets for constraints with full discrete sets.
+        /// </summary>
+        /// <param name="constraints">The constraints.</param>
+        /// <param name="size">The size.</param>
+        /// <returns></returns>
+        public static List<Constraint> createConstraintSets_FullDiscreteSets(List<Constraint> constraints, int size)
+        {
+            foreach (Constraint constraint in constraints) 
+            {
+                createConstraintSet_FullDiscreteSets(constraint, size);
+            }
+            return constraints;
+        }
+
+        /// <summary>
+        /// Creates the Set for constraint with full discrete set.
+        /// </summary>
+        /// <param name="constraint">The constraint.</param>
+        /// <param name="size">The size.</param>
+        private static void createConstraintSet_FullDiscreteSets(Constraint constraint, int sizeOfFullDiscreteSet)
+        {
+            // create Set for constraint with full discrete set
+            constraint.DiscreteSet = new Set(createSequenceOfNumber(sizeOfFullDiscreteSet), MODULO_DEFAULT);
+            // create minimization factor for appropriate discrete set
+            // according the amount of passenger on transfer
+            constraint.DiscreteSet.createMinimizationFactor(constraint.Transfer.Passengers);
+        }
+
 		/// <summary>
-		/// Creates the Sets for constraints with different period.
+		/// Creates the Sets for constraints with respect to period T of both lines on Constraint.
 		/// </summary>
 		/// <param name="constraints">The constraints.</param>
 		/// <param name="size">The size of set.</param>
 		/// <returns>List of Constraints.</returns>
-		public static List<Constraint> createConstraintSetsWithDifferentPeriod(List<Constraint> constraints, int size)
+        public static List<Constraint> createConstraintSets_AlfaTTransferTime(List<Constraint> constraints, int size)
 		{
 			foreach (Constraint constraint in constraints)
 			{
-				//createConstraintSet(constraint, size);
-				createConstraintSetWithDifferentPeriods(constraint, size);
+				createConstraintSet_AlfaTTransferTime(constraint, size);
 			}
 			return constraints;
 		}
 
 
 		/// <summary>
-		/// Creates the Set for constraint with different periods.
+        /// Creates the Set for constraint with respect to period T of both lines on Constraint.
 		/// Calculate with frequency of within Modulo Model.
+        /// Maximal transfer time is then alfa*periodLine for all transfers.
 		/// </summary>
 		/// <param name="constraint">The constraint.</param>
 		/// <param name="size">The size of set.</param>
-		public static void createConstraintSetWithDifferentPeriods(Constraint constraint, int size)
+		public static void createConstraintSet_AlfaTTransferTime(Constraint constraint, int size)
 		{
 			List<Set> newSets = new List<Set>();
 			// get minimal period
 			int period = Math.Min((int)constraint.TrainLine1.Period, (int)constraint.TrainLine2.Period);
+            // calculate frequency
+            int frequency = MODULO_DEFAULT / period;
 			// create as many seqence as they are within modulo
-			for (int i = 0; i < MODULO_DEFAULT / period; i++)
+			for (int i = 0; i < frequency; i++)
 			{
-				// new set {60*i .. 60*i + size/modulo_def/period - 1}
-				Set newSet = new Set(createSequenceOfNumber(i * period, i * period + size / (MODULO_DEFAULT / period) - 1), MODULO_DEFAULT);
+				// new set {60*i .. 60*i + size/frequency - 1}, transfer time is shorten by frequency
+				Set newSet = new Set(createSequenceOfNumber(i * period, i * period + size / (frequency) - 1), MODULO_DEFAULT);
 				// new minfactor for set
 				newSet.createMinimizationFactor(constraint.Transfer.Passengers);
 				// add it
