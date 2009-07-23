67dd5fd979d1238b53eb25906fdd8ec82b196865
kalicky <kalicky@62c59678-76e0-11de-91a4-c5220aa94298>
2009-07-23 15:32:11 +0000

    Update: Implemented methods of classes, which implements IBestChoiceSearcher.

--- trunk/trains/Trains/Interfaces/IBestChoiceSearcher.cs
+++ trunk/trains/Trains/Interfaces/IBestChoiceSearcher.cs
@@ -6,6 +6,34 @@ namespace PeriodicTimetableGeneration.Interfaces
 {
     public interface IBestChoiceSearcher
     {
-        FactorRangeRecord chooseBestChoice(Set[,] matrix);
+        Boolean chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord);
     }
+
+    public class DeterministicSearcher : IBestChoiceSearcher 
+    {
+
+
+        #region IBestChoiceSearcher Members
+
+        public bool chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord)
+        {
+            return BestSearchUtil.deterministicSearchForBestRecord(matrix, out resultFactorRangeRecord);
+        }
+
+        #endregion
+    }
+
+    public class ProbableSearcher : IBestChoiceSearcher 
+    {
+
+        #region IBestChoiceSearcher Members
+
+        public bool chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord)
+        {
+            return BestSearchUtil.probableSearchForBestRecord(matrix, out resultFactorRangeRecord);
+        }
+
+        #endregion
+    }
+
 }
