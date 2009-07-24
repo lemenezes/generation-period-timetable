f28dc2e8da221480a264a53dbc632b378828d866
kalicky <kalicky@62c59678-76e0-11de-91a4-c5220aa94298>
2009-07-24 11:36:58 +0000

    Update: Comments added.

--- trains/Trains/Interfaces/IBestChoiceSearcher.cs
+++ trains/Trains/Interfaces/IBestChoiceSearcher.cs
@@ -4,36 +4,64 @@ using System.Text;
 
 namespace PeriodicTimetableGeneration.Interfaces
 {
+    /// <summary>
+    /// Interface for best choice searcher.
+    /// </summary>
     public interface IBestChoiceSearcher
     {
+        /// <summary>
+        /// Chooses the best record, the proper Set, from matrix and store into result factor range record. 
+        /// </summary>
+        /// <param name="matrix">The matrix.</param>
+        /// <param name="resultFactorRangeRecord">The result factor range record.</param>
+        /// <returns></returns>
         Boolean chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord);
     }
 
-    public class DeterministicSearcher : IBestChoiceSearcher 
+    namespace BestChoiceSearchers
     {
 
-
-        #region IBestChoiceSearcher Members
-
-        public bool chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord)
+        /// <summary>
+        /// Deterministic searcher implements IBestChoiceSearcher interface.
+        /// </summary>
+        public class DeterministicSearcher : IBestChoiceSearcher
         {
-            return BestSearchUtil.deterministicSearchForBestRecord(matrix, out resultFactorRangeRecord);
+
+            #region IBestChoiceSearcher Members
+
+            /// <summary>
+            /// Chooses the best record, the proper Set, from matrix and store into result factor range record.
+            /// </summary>
+            /// <param name="matrix">The matrix.</param>
+            /// <param name="resultFactorRangeRecord">The result factor range record.</param>
+            /// <returns></returns>
+            public bool chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord)
+            {
+                // use deterministic choice
+                return BestSearchUtil.deterministicSearchForBestRecord(matrix, out resultFactorRangeRecord);
+            }
+
+            #endregion
         }
 
-        #endregion
-    }
-
-    public class ProbableSearcher : IBestChoiceSearcher 
-    {
-
-        #region IBestChoiceSearcher Members
-
-        public bool chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord)
+        public class ProbableSearcher : IBestChoiceSearcher
         {
-            return BestSearchUtil.probableSearchForBestRecord(matrix, out resultFactorRangeRecord);
+
+            #region IBestChoiceSearcher Members
+
+            /// <summary>
+            /// Chooses the best record, the proper Set, from matrix and store into result factor range record.
+            /// </summary>
+            /// <param name="matrix">The matrix.</param>
+            /// <param name="resultFactorRangeRecord">The result factor range record.</param>
+            /// <returns></returns>
+            public bool chooseBestRecord(Set[,] matrix, out FactorRangeRecord resultFactorRangeRecord)
+            {
+                // use probablistic choice
+                return BestSearchUtil.probableSearchForBestRecord(matrix, out resultFactorRangeRecord);
+            }
+
+            #endregion
         }
-
-        #endregion
     }
-
 }
