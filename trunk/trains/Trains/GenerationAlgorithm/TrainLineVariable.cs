81d3adde16c3937f11c66f8b5ba8e56e8bbcd154
kalicky <kalicky@62c59678-76e0-11de-91a4-c5220aa94298>
2009-07-24 07:46:42 +0000

    Update: Comments added and refactoring code.

--- trunk/trains/Trains/GenerationAlgorithm/TrainLineVariable.cs
+++ trunk/trains/Trains/GenerationAlgorithm/TrainLineVariable.cs
@@ -2,36 +2,69 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace PeriodicTimetableGeneration
 {
+    /// <summary>
+    /// Class represents a train line with specified start time in particular timetable.
+    /// </summary>
     public class TrainLineVariable
     {
-        private TrainLine trainLine;
-        private Time startTime;
-        /// <summary>
-        /// value of minimalization factor
-        /// </summary>
-        private int ratingValue;
-        private int progressiveChanges;
-        private Boolean fixedFlag;
+
+        #region Prvate Fields
 
         /// <summary>
-        /// constructor
+        /// Train line, which this trainLineVariable is related to.
         /// </summary>
-        public TrainLineVariable()
+        private TrainLine trainLine;
+        /// <summary>
+        /// Start time of train line in particular timetable, which holds this instance.
+        /// </summary>
+        private Time startTime;
+        /// <summary>
+        /// Value of minimalization factor of this line.
+        /// </summary>
+        private int ratingValue;
+        /// <summary>
+        /// The number represents number of progressive changes, which has been made.
+        /// </summary>
+        private int progressiveChanges;
+
+        ///// <summary>
+        ///// Flag indicates fixation (not implemented).
+        ///// </summary>
+        //private Boolean fixedFlag;
+
+        #endregion
+
+        #region Constructors
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrainLineVariable"/> class.
+        /// </summary>
+        private TrainLineVariable()
         {
             throw new System.NotImplementedException();
         }
 
         /// <summary>
-        /// constructor
+        /// Initializes a new instance of the <see cref="TrainLineVariable"/> class.
         /// </summary>
+        /// <param name="line">The line.</param>
         public TrainLineVariable(TrainLine line)
         {
             setDefaultValue();
             trainLine = line;
         }
 
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the start time.
+        /// </summary>
+        /// <value>The start time.</value>
         public Time StartTime
         {
             get
@@ -44,29 +77,34 @@ namespace PeriodicTimetableGeneration
             }
         }
 
+        /// <summary>
+        /// Gets the line.
+        /// </summary>
+        /// <value>The train line.</value>
         public TrainLine Line
         {
             get
             {
                 return trainLine;
             }
-            set
-            {
-                //trainLine1 = value;
-            }
         }
 
+        /// <summary>
+        /// Gets the line number.
+        /// </summary>
+        /// <value>The line number.</value>
         public int LineNumber
         {
             get
             {
                 return trainLine.LineNumber;
             }
-            set
-            {
-            }
         }
 
+        /// <summary>
+        /// Gets or sets the rating value.
+        /// </summary>
+        /// <value>The rating value.</value>
         public int RatingValue
         {
             get
@@ -79,6 +117,10 @@ namespace PeriodicTimetableGeneration
             }
         }
 
+        /// <summary>
+        /// Gets or sets the progressive changes.
+        /// </summary>
+        /// <value>The progressive changes.</value>
         public int ProgressiveChanges
         {
             get
@@ -91,41 +133,51 @@ namespace PeriodicTimetableGeneration
             }
         }
 
-        public Boolean FixedFlag
-        {
-            get
-            {
-                return fixedFlag;
-            }
-            set
-            {
-                fixedFlag = value;
-            }
-        }
+        //public Boolean FixedFlag
+        //{
+        //    get
+        //    {
+        //        return fixedFlag;
+        //    }
+        //    set
+        //    {
+        //        fixedFlag = value;
+        //    }
+        //}
 
+        /// <summary>
+        /// Gets the period of this line.
+        /// </summary>
+        /// <value>The period.</value>
         public Period Period
         {
             get
             {
                 return trainLine.Period;
             }
-            set
-            {
-            }
         }
 
+        /// <summary>
+        /// Gets the type of train of this line.
+        /// </summary>
+        /// <value>The type of train.</value>
         public TypeOfTrain TypeOfTrain
         {
             get
             {
                 return trainLine.TypeTrain;
             }
-            set
-            {
-            }
         }
 
+        #endregion
 
+        #region Public Methods
+
+        /// <summary>
+        /// Returns arrival on specified station within this line.
+        /// </summary>
+        /// <param name="station">The station.</param>
+        /// <returns></returns>
         public Time arrivalOnStation(TrainStation station)
         {
             TrainStop stop =  trainLine.getTrainStopOnStation(station.Name);
@@ -149,6 +201,11 @@ namespace PeriodicTimetableGeneration
             return time;
         }
 
+        /// <summary>
+        /// Returns departures from specified station with in this line.
+        /// </summary>
+        /// <param name="station">The station.</param>
+        /// <returns>The arrival time.</returns>
         public Time departureFromStation(TrainStation station) 
         {
             TrainStop stop = trainLine.getTrainStopOnStation(station.Name);
@@ -173,8 +230,19 @@ namespace PeriodicTimetableGeneration
             return time;
         }
 
+        //public Boolean isFixed()
+        //{
+        //    return fixedFlag;
+        //}
+
+        #endregion
+
+
+        #region Private Methods
+
+
         /// <summary>
-        /// set default value
+        /// Sets the default value for fields of this instance.
         /// </summary>
         private void setDefaultValue()
         {
@@ -182,12 +250,12 @@ namespace PeriodicTimetableGeneration
             startTime = Time.MinValue;
             ratingValue = int.MaxValue;
             progressiveChanges = 0;
-            fixedFlag = false;
+            //fixedFlag = false;
         }
 
-        public Boolean isFixed()
-        {
-            return fixedFlag;
-        }
+        #endregion
+
+
+
     }
 }
