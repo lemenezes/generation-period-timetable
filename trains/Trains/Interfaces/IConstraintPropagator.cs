ec27488bf4cc5b7e771c2ba7aba49f05f4ae1860
kalicky <kalicky@62c59678-76e0-11de-91a4-c5220aa94298>
2009-07-24 11:57:50 +0000

    Update: Comments added.

--- trains/Trains/Interfaces/IConstraintPropagator.cs
+++ trains/Trains/Interfaces/IConstraintPropagator.cs
@@ -5,92 +5,159 @@ using PeriodicTimetableGeneration.Interfaces.ConstraintSetsCreators;
 
 namespace PeriodicTimetableGeneration.Interfaces
 {
+    /// <summary>
+    /// Interface for constraint propagator.
+    /// </summary>
 	public interface IConstraintPropagator
 	{
+        /// <summary>
+        /// Runs the propagation algorithm.
+        /// </summary>
+        /// <param name="constraints">The constraints.</param>
+        /// <param name="size">The size.</param>
+        /// <returns></returns>
 		PropagationResult runPropagationAlgorithm(List<Constraint> constraints, int size);
 	}
 
-	public class BisectionPropagator : IConstraintPropagator
-	{
+    namespace ConstraintPropagators
+    {
+        /// <summary>
+        /// Bisection propagator implements the IConstraintPropagator interface.
+        /// </summary>
+        public class BisectionPropagator : IConstraintPropagator
+        {
 
-		#region Contraint sets creator
+            #region Contraint sets creator
 
-		private readonly IConstraintSetsCreator constraintSetsCreator;
+            /// <summary>
+            /// Constraint set creator used in call of runPropagationAlgorithm method.
+            /// </summary>
+            private readonly IConstraintSetsCreator constraintSetsCreator;
 
-		protected IConstraintSetsCreator ConstraintSetsCreator
-		{
-			get
-			{
-				return this.constraintSetsCreator;
-			}
-		}
+            #endregion
 
-		#endregion
+            #region Default settings for the algorithm
+
+            /// <summary>
+            /// Default value for lower bound used in Bisection Algorithm.
+            /// </summary>
+            private const int LOWER_BOUND_DEFAULT = 0;
+
+            /// <summary>
+            /// Default value for upper bound used in Bisection Algorithm.
+            /// </summary>
+            private const int UPPER_BOUND_DEFAULT = 120;
+
+            #endregion
+
+            #region Constructor
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="BisectionPropagator"/> class.
+            /// </summary>
+            /// <param name="constraintSetsCreator">The constraint sets creator.</param>
+            public BisectionPropagator(IConstraintSetsCreator constraintSetsCreator)
+            {
+                // initialize
+                this.constraintSetsCreator = constraintSetsCreator;
+            }
+
+            #endregion
 
 
-        #region Constructor
+            #region IConstraintPropagator Members
 
-        public BisectionPropagator(IConstraintSetsCreator constraintSetsCreator)
-		{
-			this.constraintSetsCreator = constraintSetsCreator;
+            /// <summary>
+            /// Runs the propagation algorithm.
+            /// </summary>
+            /// <param name="constraints">The constraints.</param>
+            /// <param name="size">The size.</param>
+            /// <returns></returns>
+            public PropagationResult runPropagationAlgorithm(List<Constraint> constraints, int size)
+            {
+                // bisection algorithm is used for determine size of Sets and calls PropagationUtil.runPropagationAlgorithm
+                return BisectionUtil.Bisect(constraints, this.ConstraintSetsCreator, LOWER_BOUND_DEFAULT, UPPER_BOUND_DEFAULT);
+            }
+
+            #endregion
+
+            #region Properties
+
+            /// <summary>
+            /// Gets the constraint sets creator.
+            /// </summary>
+            /// <value>The constraint sets creator.</value>
+            protected IConstraintSetsCreator ConstraintSetsCreator
+            {
+                get
+                {
+                    return this.constraintSetsCreator;
+                }
+            }
+
+            #endregion
         }
 
-        #endregion
-
-
-        #region Default settings for the algorithm
-
         /// <summary>
-		/// Default value for lower bound used in Bisection Algorithm.
-		/// </summary>
-		private const int LOWER_BOUND_DEFAULT = 0;
+        /// Simple propagator implements IConstraintPropagator interface.
+        /// </summary>
+        public class SimplePropagator : IConstraintPropagator
+        {
+            #region Contraint sets creator
 
-		/// <summary>
-		/// Default value for upper bound used in Bisection Algorithm.
-		/// </summary>
-		private const int UPPER_BOUND_DEFAULT = 120;
+            /// <summary>
+            /// Constraint set creator used in call of runPropagationAlgorithm method.
+            /// </summary>
+            private readonly IConstraintSetsCreator constraintSetsCreator;
+           
+            #endregion
 
-		#endregion
+            #region Constructor
 
-		#region IConstraintPropagator Members
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SimplePropagator"/> class.
+            /// </summary>
+            /// <param name="constraintSetsCreator">The constraint sets creator.</param>
+            public SimplePropagator(IConstraintSetsCreator constraintSetsCreator)
+            {
+                // initialize
+                this.constraintSetsCreator = constraintSetsCreator;
+            }
 
-		public PropagationResult runPropagationAlgorithm(List<Constraint> constraints, int size)
-		{
-			return BisectionUtils.Bisect(constraints, this.ConstraintSetsCreator, LOWER_BOUND_DEFAULT, UPPER_BOUND_DEFAULT);
-		}
+            #endregion
 
-		#endregion
-	}
 
-	public class SimplePropagator : IConstraintPropagator
-	{
-		#region Contraint sets creator
+            #region IConstraintPropagator Members
 
-		private readonly IConstraintSetsCreator constraintSetsCreator;
+            /// <summary>
+            /// Runs the propagation algorithm.
+            /// </summary>
+            /// <param name="constraints">The constraints.</param>
+            /// <param name="size">The size.</param>
+            /// <returns></returns>
+            public PropagationResult runPropagationAlgorithm(List<Constraint> constraints, int size)
+            {
+                // calls directly PropagationUtil.runPropagationAlgorithm
+                return PropagationUtil.runPropagationAlgorithm(constraints, this.ConstraintSetsCreator, size);
+            }
 
-		protected IConstraintSetsCreator ConstraintSetsCreator
-		{
-			get
-			{
-				return this.constraintSetsCreator;
-			}
-		}
+            #endregion
 
-		#endregion
+            #region Properties
 
-		public SimplePropagator(IConstraintSetsCreator constraintSetsCreator)
-		{
-			this.constraintSetsCreator = constraintSetsCreator;
-		}
+            /// <summary>
+            /// Gets the constraint sets creator.
+            /// </summary>
+            /// <value>The constraint sets creator.</value>
+            protected IConstraintSetsCreator ConstraintSetsCreator
+            {
+                get
+                {
+                    return this.constraintSetsCreator;
+                }
+            }
 
-		#region IConstraintPropagator Members
-
-		public PropagationResult runPropagationAlgorithm(List<Constraint> constraints, int size)
-		{
-			return PropagationUtils.runPropagationAlgorithm(constraints, this.ConstraintSetsCreator, size);
-		}
-
-		#endregion
-	}
-
+            #endregion
+        }
+    }
 }
