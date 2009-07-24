a2cdb48fb3c5d41efeb4f54306c4c30cd80ae768
kalicky <kalicky@62c59678-76e0-11de-91a4-c5220aa94298>
2009-07-24 12:03:53 +0000

    Update: Comments added.

--- trunk/trains/Trains/Interfaces/IConstraintSetsCreator.cs
+++ trunk/trains/Trains/Interfaces/IConstraintSetsCreator.cs
@@ -1,48 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PeriodicTimetableGeneration.GenerationAlgorithmDSAs;
 
 namespace PeriodicTimetableGeneration.Interfaces
 {
+    /// <summary>
+    /// Infercafe for constraint sets creator.
+    /// </summary>
     public interface IConstraintSetsCreator
     {
-        List<Constraint> createConstraintSet(List<Constraint> constraints, int size);
+        /// <summary>
+        /// Creates the sets for constraints.
+        /// </summary>
+        /// <param name="constraints">The constraints.</param>
+        /// <param name="size">The size.</param>
+        /// <returns></returns>
+        List<Constraint> createConstraintSets(List<Constraint> constraints, int size);
     }
 
 	namespace ConstraintSetsCreators
 	{
 
+        /// <summary>
+        /// Create constraint sets, the same transfer time is guqranteed for all transfers.
+        /// Implements constraint sets creator.
+        /// </summary>
 		public class SameTransferTime : IConstraintSetsCreator 
 		{
 			#region IConstraintSetsCreator Members
 
-			public List<Constraint> createConstraintSet(List<Constraint> constraints, int size)
+            /// <summary>
+            /// Creates the sets for constraints.
+            /// </summary>
+            /// <param name="constraints">The constraints.</param>
+            /// <param name="size">The size.</param>
+            /// <returns></returns>
+			public List<Constraint> createConstraintSets(List<Constraint> constraints, int size)
 			{
-				return GenerationAlgorithmPESPUtil.createConstraintSets_SameTransferTime(constraints, size);
+                // same transfer time
+				return CreateConstraintSetsUtil.createConstraintSets_SameTransferTime(constraints, size);
 			}
 
 			#endregion
 		}
 
+        /// <summary>
+        /// Create constraint sets, the alfa*period transfer time is quaranteed for all transfers.
+        /// Implements constraint sets creator.
+        /// </summary>
 		public class AlfaTTransferTime : IConstraintSetsCreator
 		{
 			#region IConstraintSetsCreator Members
 
-			public List<Constraint> createConstraintSet(List<Constraint> constraints, int size)
+            /// <summary>
+            /// Creates the sets for constraints.
+            /// </summary>
+            /// <param name="constraints">The constraints.</param>
+            /// <param name="size">The size.</param>
+            /// <returns></returns>
+			public List<Constraint> createConstraintSets(List<Constraint> constraints, int size)
 			{
-				return GenerationAlgorithmPESPUtil.createConstraintSets_AlfaTTransferTime(constraints, size);
+                // alfta*period transfer time
+                return CreateConstraintSetsUtil.createConstraintSets_AlfaTTransferTime(constraints, size);
 			}
 
 			#endregion
 		}
 
+        /// <summary>
+        /// Create constraint full discrete sets.
+        /// Implements constraint sets creator.
+        /// </summary>
 		public class FullDiscreteSet : IConstraintSetsCreator
 		{
 			#region IConstraintSetsCreator Members
 
-			public List<Constraint> createConstraintSet(List<Constraint> constraints, int size)
+            /// <summary>
+            /// Creates the sets for constraints.
+            /// </summary>
+            /// <param name="constraints">The constraints.</param>
+            /// <param name="size">The size.</param>
+            /// <returns></returns>
+			public List<Constraint> createConstraintSets(List<Constraint> constraints, int size)
 			{
-				return GenerationAlgorithmPESPUtil.createConstraintSets_FullDiscreteSets(constraints, size);
+                // no transfer time is quaranteed
+                return CreateConstraintSetsUtil.createConstraintSets_FullDiscreteSets(constraints, size);
 			}
 
 			#endregion
